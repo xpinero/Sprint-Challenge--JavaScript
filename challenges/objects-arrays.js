377095aac163ea6dadd38701234ba879899c5c03
Xaver Pinero <xaverpinero@Zacharys-MacBook-Pro-2.local>
2020-01-26 19:50:20 -0500

    made objects

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -5,11 +5,29 @@
   Use this pattern to create your objects: 
   object name, diet, weight, length, period
 */
-
+const tyrannosaurus = {
+  name: "tyrannosaurus",
+  diet: "carnivorous",
+  weight: "7000kg",
+  length: "12m",
+  period: "Late Cretaceous"
+};
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
-
+const stegosaurus = {
+  name: "stegosaurus",
+  diet: "carnivorous",
+  weight: "2000kg",
+  length: "9m",
+  period: "Late Jurassic"
+};
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+const velociraptor = {
+  name: "velociraptor",
+  diet: "carnivorous",
+  weight: "15kg",
+  length: "1.8m",
+  period: "Late Cretaceous"
+};
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
 
 // Using your dinosaur objects, log answers to these questions:
